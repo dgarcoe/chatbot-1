e18ad1976479f79708f6a8c2b2fe2bf0c71a57b4
Daniel García Coego <dannn86m@gmail.com>
2024-09-20 07:27:37 +0000

    First feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,10 @@ if prompt := st.chat_input("¡Pregúntame cualquier duda que tengas!"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Test"
+    response = f"Sanxenxo es conocido por sus hermosas playas. Aquí tienes tres de las más destacadas:\n"+"Playa de Silgar: Esta es la playa más famosa y concurrida de Sanxenxo. Situada en el centro de la ciudad, es ideal para quienes buscan comodidad y servicios cercanos.\n"+"Playa de Areas: Un poco más tranquila que Silgar, esta playa ofrece aguas cristalinas y arena fina. Es perfecta para relajarse y disfrutar de un entorno más natural.\n"+"Playa de Montalvo: Ubicada en un entorno más rural, esta playa es conocida por su belleza natural y su ambiente más sereno. Es ideal para quienes buscan escapar del bullicio."
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.audio()
+        st.audio("./resources/cat.mp3",format="audio/mpeg",loop=True)
         st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
